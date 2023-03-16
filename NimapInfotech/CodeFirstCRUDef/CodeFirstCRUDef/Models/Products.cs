9bfd1cc4f8efe09800e0cf303461dcf5fe7299bb
AkashKondhalkar13 <akash503011@gmail.com>
2023-03-16 12:23:01 +0530

    EFCRUD

---
+++ NimapInfotech/CodeFirstCRUDef/CodeFirstCRUDef/Models/Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstCRUDef.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage ="Product Name Required.")]
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "Category ID Required.")]
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Category Name  Required.")]
+        public string CategoryName { get; set; }
+    }
+    public class EFCodeFirst : DbContext
+    {
+        public DbSet<Products> products { get; set; }
+    }
+}
